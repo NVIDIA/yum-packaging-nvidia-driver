7b998b87b20b522d9628ecb1d23a8c266cb33555
Alex Dibbley <adibbley@nvidia.com>
2018-09-14 12:57:22 -0700

    Removed tls directory

--- nvidia-generate-tarballs.sh
+++ nvidia-generate-tarballs.sh
@@ -1,67 +1,70 @@
 #!/bin/sh
 set -e
 
-VERSION=${VERSION:-460.56}
+VERSION=${VERSION:-390.48}
 DL_SITE=${DL_SITE:-http://us.download.nvidia.com/XFree86}
 TEMP_UNPACK=${TEMP_UNPACK:-temp}
 
-PLATFORM=Linux-x86_64
-RUN_FILE=NVIDIA-${PLATFORM}-${VERSION}.run
+get_run_file() {
+    printf "Downloading installer for ${VERSION} ${ARCH}... "
+    [[ -f $RUN_FILE ]] || wget -c -q ${DL_SITE}/${PLATFORM}/${VERSION}/$RUN_FILE
+    printf "OK\n"
+}
 
-printf "Downloading installer ${RUN_FILE}... "
-[[ -f $RUN_FILE ]] || wget -c -q ${DL_SITE}/${PLATFORM}/${VERSION}/$RUN_FILE
-printf "OK\n"
+extract_run_file() {
+    sh ${RUN_FILE} --extract-only --target ${TEMP_UNPACK}
+}
 
-sh ${RUN_FILE} --extract-only --target ${TEMP_UNPACK}
+create_tarball() {
+    printf "Creating tarballs for ${VERSION} ${ARCH}... "
 
-printf "Cleaning up binaries... "
+    KMOD=nvidia-kmod-${VERSION}-${ARCH}
+    DRIVER=nvidia-driver-${VERSION}-${ARCH}
+    mkdir ${KMOD} ${DRIVER}
 
-cd ${TEMP_UNPACK}
+    cd ${TEMP_UNPACK}
 
-# Compiled from source
-rm -f \
-    nvidia-xconfig* \
-    nvidia-persistenced* \
-    nvidia-modprobe* \
-    libnvidia-gtk* nvidia-settings* \
-    libGLESv1_CM.so.* libGLESv2.so.* libGL.la libGLdispatch.so.* libOpenGL.so.* libGLX.so.* libGL.so.1* libEGL.so.1* \
-    libnvidia-egl-wayland.so.* \
-    libOpenCL.so.1* \
-    32/libGLESv1_CM.so.* 32/libGLESv2.so.* 32/libGL.la 32/libGLdispatch.so.* 32/libOpenGL.so.* 32/libGLX.so.* 32/libGL.so.1* 32/libEGL.so.1* \
-    32/libOpenCL.so.1*
+    # Compiled from source
+    rm -f \
+        nvidia-xconfig* \
+        nvidia-persistenced* \
+        nvidia-modprobe* \
+        libnvidia-gtk* nvidia-settings* \
+        libGLESv1_CM.so.* libGLESv2.so.* libGL.la libGLdispatch.so.* libOpenGL.so.* libGLX.so.* libGL.so.1* libEGL.so.1* \
+        libnvidia-egl-wayland.so.* \
+        libOpenCL.so.1*
 
-# Non GLVND libraries
-rm -f \
-    libGL.so.${VERSION} libEGL.so.${VERSION} \
-    32/libGL.so.${VERSION} 32/libEGL.so.${VERSION}
+    # Non GLVND libraries
+    rm -f libGL.so.${VERSION} libEGL.so.${VERSION}
 
-# Useless with packages
-rm -f nvidia-installer* .manifest make* mk* tls_test*
+    # Useless with packages
+    rm -f nvidia-installer* .manifest make* mk* tls_test*
 
-# Add json files in both architectures
-cp -f *.json* 32/
+    # useless on modern distributions
+    rm -f libnvidia-wfb*
 
-cd ..
+    mv kernel ../${KMOD}/
+    mv * ../${DRIVER}/
 
-KMOD=nvidia-kmod-${VERSION}-x86_64
-USR_64=nvidia-driver-${VERSION}-x86_64
-USR_32=nvidia-driver-${VERSION}-i386
+    cd ..
+    rm -fr ${TEMP_UNPACK}
 
-mkdir ${KMOD} ${USR_64} ${USR_32}
-mv ${TEMP_UNPACK}/kernel ${KMOD}/
-mv ${TEMP_UNPACK}/32/* ${USR_32}/
-mv ${TEMP_UNPACK}/* ${USR_64}/
-
-rm -fr ${TEMP_UNPACK}
-
-printf "OK\n"
-
-for tarball in ${KMOD} ${USR_32} ${USR_64}; do
-
-    printf "Creating tarball $tarball... "
-
-    tar --remove-files -cJf $tarball.tar.xz $tarball
+    tar --remove-files -cJf ${KMOD}.tar.xz ${KMOD}
+    tar --remove-files -cJf ${DRIVER}.tar.xz ${DRIVER}
 
     printf "OK\n"
+}
 
-done
+ARCH=i386
+PLATFORM=Linux-x86
+RUN_FILE=NVIDIA-${PLATFORM}-${VERSION}.run
+get_run_file
+extract_run_file
+create_tarball
+
+ARCH=x86_64
+PLATFORM=Linux-${ARCH}
+RUN_FILE=NVIDIA-${PLATFORM}-${VERSION}-no-compat32.run
+get_run_file
+extract_run_file
+create_tarball
