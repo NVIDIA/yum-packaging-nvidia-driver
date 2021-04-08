b443ee749138c5c93249121dbc20b6a4d730aea8
Kevin Mittman <kmittman@nvidia.com>
2021-04-08 14:25:50 -0700

    Add unofficial aarch64 support for RHEL/CentOS 7
    Signed-off-by: Kevin Mittman <kmittman@nvidia.com>

--- nvidia-generate-tarballs-aarch64.sh
+++ nvidia-generate-tarballs-aarch64.sh
@@ -36,7 +36,6 @@ create_tarball() {
         libnvidia-gtk* nvidia-settings* \
         libGLESv1_CM.so.* libGLESv2.so.* libGL.la libGLdispatch.so.* libOpenGL.so.* libGLX.so.* libGL.so.1* libEGL.so.1* \
         libnvidia-egl-wayland.so.* \
-        libnvidia-vulkan-producer.so.* \
         libOpenCL.so.1*
 
     # Non GLVND libraries
