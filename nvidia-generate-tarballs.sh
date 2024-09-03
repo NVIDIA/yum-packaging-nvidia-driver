d54fc9a8ec536dbfbbc09c6f23dfd5d89e4572f7
Simone Caronni <scaronni@nvidia.com>
2024-09-03 13:25:01 +0200

    Major rework

--- nvidia-generate-tarballs.sh
+++ nvidia-generate-tarballs.sh
@@ -1,72 +1,103 @@
 #!/bin/sh
 set -e
 
-VERSION=${VERSION:-430.14}
-DL_SITE=${DL_SITE:-http://us.download.nvidia.com/XFree86}
-TEMP_UNPACK=${TEMP_UNPACK:-temp}
+set_vars() {
+   export VERSION=${VERSION:-550.54.14}
+   export DL_SITE=${DL_SITE:-http://download.nvidia.com/XFree86}
+   export TEMP_UNPACK=${ARCH}
+   export PLATFORM=Linux-${ARCH}
+   export RUN_FILE=${RUN_FILE:-NVIDIA-${PLATFORM}-${VERSION}.run}
+}
 
-RUN_FILE=${RUN_FILE:-NVIDIA-${PLATFORM}-${VERSION}.run}
+run_file_get() {
+    printf "Downloading installer ${RUN_FILE}... "
+    [[ -f $RUN_FILE ]] || wget -c -q ${DL_SITE}/${PLATFORM}/${VERSION}/$RUN_FILE
+    printf "OK\n"
+}
 
-printf "Downloading installer ${RUN_FILE}... "
-[[ -f $RUN_FILE ]] || wget -c -q ${DL_SITE}/${PLATFORM}/${VERSION}/$RUN_FILE
-printf "OK\n"
+run_file_extract() {
+    rm -fr ${TEMP_UNPACK}
+    sh ${RUN_FILE} --extract-only --target ${TEMP_UNPACK}
+}
 
-sh ${RUN_FILE} --extract-only --target ${TEMP_UNPACK}
+cleanup_folder() {
 
-printf "Cleaning up binaries... "
+    printf "Cleaning up binaries... "
 
-cd ${TEMP_UNPACK}
+    cd ${TEMP_UNPACK}
 
-# Compiled from source
-rm -f \
-    nvidia-xconfig* \
-    nvidia-persistenced* \
-    nvidia-modprobe* \
-    libnvidia-gtk* nvidia-settings* \
-    libGLESv1_CM.so.* libGLESv2.so.* libGL.la libGLdispatch.so.* libOpenGL.so.* libGLX.so.* libGL.so.1* libEGL.so.1* \
-    libnvidia-egl-wayland.so.* \
-    libOpenCL.so.1* \
-    32/libGLESv1_CM.so.* 32/libGLESv2.so.* 32/libGL.la 32/libGLdispatch.so.* 32/libOpenGL.so.* 32/libGLX.so.* 32/libGL.so.1* 32/libEGL.so.1* \
-    32/libOpenCL.so.1*
+    # Stuff not needed for packages:
+    #   - Compiled from source
+    #   - Interactive installer files
+    #   - GLVND GL libraries
+    #   - Internal development only libraries
+    rm -fr \
+        nvidia-xconfig* \
+        nvidia-persistenced* \
+        nvidia-modprobe* \
+        libnvidia-gtk* libnvidia-wayland-client* nvidia-settings* \
+        libGLESv1_CM.so.* libGLESv2.so.* libGLdispatch.so.* libOpenGL.so.* libGLX.so.* libGL.so.1* libEGL.so.1* \
+        libnvidia-egl-wayland.so.* libnvidia-egl-gbm.so.* \
+        libOpenCL.so.1* \
+        libGL.so.${VERSION} libEGL.so.${VERSION} \
+        nvidia-installer* .manifest make* mk* tls_test* libglvnd_install_checker \
+        libnvidia-llvmnvgpu.so.*
 
-# Non GLVND libraries
-rm -f \
-    libGL.so.${VERSION} libEGL.so.${VERSION} \
-    32/libGL.so.${VERSION} 32/libEGL.so.${VERSION}
+    if [ "${ARCH}" == x86_64 ]; then
+        rm -fr \
+          32/libGLESv1_CM.so.* 32/libGLESv2.so.* 32/libGLdispatch.so.* 32/libOpenGL.so.* 32/libGLX.so.* 32/libGL.so.1* 32/libEGL.so.1* \
+          32/libOpenCL.so.1* \
+          32/libGL.so.${VERSION} 32/libEGL.so.${VERSION} \
+          32/libnvidia-egl-wayland.so.* 32/libnvidia-egl-gbm.so.*
 
-# Useless with packages
-rm -f nvidia-installer* .manifest make* mk* tls_test*
+        cp -f *.json* 32/
+    fi
 
-# useless on modern distributions
-rm -f libnvidia-wfb*
-
-# Add json files in both architectures
-cp -f *.json* 32/
-
-cd ..
-
-KMOD_OPEN=nvidia-open-kmod-${VERSION}-x86_64
-KMOD_LEGACY=nvidia-kmod-${VERSION}-x86_64
-USR_64=nvidia-driver-${VERSION}-x86_64
-USR_32=nvidia-driver-${VERSION}-i386
-
-mkdir ${KMOD_OPEN} ${KMOD_LEGACY} ${USR_64} ${USR_32}
-[[ -d "${TEMP_UNPACK}/kernel-open" ]] &&
-mv ${TEMP_UNPACK}/kernel-open ${KMOD_OPEN}/
-mv ${TEMP_UNPACK}/kernel ${KMOD_LEGACY}/
-mv ${TEMP_UNPACK}/32/* ${USR_32}/
-mv ${TEMP_UNPACK}/* ${USR_64}/
-
-rm -fr ${TEMP_UNPACK}
-
-printf "OK\n"
-
-for tarball in ${KMOD_OPEN} ${KMOD_LEGACY} ${USR_32} ${USR_64}; do
-
-    printf "Creating tarball $tarball... "
-    [[ -d "$tarball" ]] &&
-    tar --remove-files -cJf $tarball.tar.xz $tarball
+    cd ..
 
     printf "OK\n"
+}
 
-done
+create_tarball() {
+
+    KMOD_OPEN=nvidia-open-kmod-${VERSION}-${ARCH}
+    KMOD_LEGACY=nvidia-kmod-${VERSION}-${ARCH}
+    KMOD_COMMON=nvidia-kmod-common-${VERSION}
+    USR_64=nvidia-driver-${VERSION}-${ARCH}
+
+    mkdir ${KMOD_LEGACY} ${KMOD_OPEN} ${KMOD_COMMON} ${USR_64}
+    mv ${TEMP_UNPACK}/kernel-open ${KMOD_OPEN}/
+    mv ${TEMP_UNPACK}/kernel ${KMOD_LEGACY}/
+    mv ${TEMP_UNPACK}/firmware ${KMOD_COMMON}/
+
+    if [ "$ARCH" == x86_64 ]; then
+
+        USR_32=nvidia-driver-${VERSION}-i386
+
+        mkdir ${USR_32}
+        mv ${TEMP_UNPACK}/32/* ${USR_32}/
+        rm -fr ${TEMP_UNPACK}/32
+
+    fi
+
+    mv ${TEMP_UNPACK}/* ${USR_64}/
+
+    rm -fr ${TEMP_UNPACK}
+
+    for tarball in ${KMOD_OPEN} ${KMOD_LEGACY} ${KMOD_COMMON} ${USR_64} ${USR_32}; do
+
+        printf "Creating tarball $tarball... "
+
+        XZ_OPT='-T0' tar --remove-files -cJf $tarball.tar.xz $tarball
+
+        printf "OK\n"
+
+    done
+}
+
+ARCH=x86_64
+set_vars
+run_file_get
+run_file_extract
+cleanup_folder
+create_tarball
